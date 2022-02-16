fbcc95ef7e97dd2f2b43115170853add02aff1f9
raulmori <47406865+raulmori@users.noreply.github.com>
2022-02-16 05:49:40 -0500

    Create Lec11 Ex20.py

---
+++ Lec11 Ex20.py
@@ -0,0 +1,25 @@
+# Customer class
+class Customer:
+    def _ _init_ _(self, name, address, phone):
+        self._ _name = name
+        self._ _address = address
+        self._ _phone = phone
+
+    def set_name(self, name):
+        self._ _name = name
+
+    def set_address(self, address):
+       self._ _address = address
+
+    def set_phone(self, phone):
+        self._ _phone = phone
+
+    def get_name(self):
+       return self._ _name
+
+    def get_address(self):
+        return self._ _address
+
+    def get_phone(self):
+        return self._ _phone
+
