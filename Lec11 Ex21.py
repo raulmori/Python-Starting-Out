cd59f833d14dce1ecdd626617cc7e083221396dd
raulmori <47406865+raulmori@users.noreply.github.com>
2022-02-16 05:49:39 -0500

    Create Lec11 Ex21.py

---
+++ Lec11 Ex21.py
@@ -0,0 +1,24 @@
+# Car class
+class Car:
+    def _ _init_ _(self, make, model, year):
+        self._ _make = make
+        self._ _model = model
+        self._ _year = year
+
+    def set_make(self, make):
+        self._ _make = make
+
+    def set_model(self, model):
+       self._ _model = model
+
+    def set_year(self, year):
+        self._ _year = year
+
+    def get_make(self):
+        return self._ _make
+
+    def get_model(self):
+        return self._ _model
+
+    def get_year(self):
+       return self._ _year
