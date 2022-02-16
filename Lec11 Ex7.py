6c11dce18f0b5c15c432c9b2a4850c9eecdb546a
raulmori <47406865+raulmori@users.noreply.github.com>
2022-02-16 05:49:47 -0500

    Create Lec11 Ex7.py

---
+++ Lec11 Ex7.py
@@ -0,0 +1,32 @@
+# The BankAccount class simulates a bank account.
+
+class BankAccount:
+
+    # The _ _init_ _ method accepts an argument for
+    # the account's balance. It is assigned to
+    # the _ _balance attribute.
+
+    def _ _init_ _(self, bal):
+        self._ _balance = bal
+
+    # The deposit method makes a deposit into the
+    # account.
+
+    def deposit(self, amount):
+        self._ _balance += amount
+
+    # The withdraw method withdraws an amount
+    # from the account.
+
+    def withdraw(self, amount):
+        if self._ _balance >= amount:
+           self._ _balance -= amount
+        else:
+            print('Error: Insufficient funds')
+
+    # The get_balance method returns the
+    # account balance.
+
+    def get_balance(self):
+        return self._ _balance
+
