42ac234e3be6dcfc3110a0c973c8acffe87da47a
raulmori <47406865+raulmori@users.noreply.github.com>
2022-02-16 20:10:01 -0500

    Update Lec13 Ex3.py

--- Lec13 Ex3.py
+++ Lec13 Ex3.py
@@ -2,23 +2,23 @@
 # the factorial of a number.
 
 def main():
-# Get a number from the user.
-number = int(input('Enter a nonnegative integer: '))
+    # Get a number from the user.
+    number = int(input('Enter a nonnegative integer: '))
 
-# Get the factorial of the number.
-fact = factorial(number)
+    # Get the factorial of the number.
+    fact = factorial(number)
 
-# Display the factorial.
-print('The factorial of', number, 'is', fact)
+    # Display the factorial.
+    print('The factorial of', number, 'is', fact)
 
 # The factorial function uses recursion to
 # calculate the factorial of its argument,
 # which is assumed to be nonnegative.
 def factorial(num):
-if num == 0:
-return 1
-else:
-return num * factorial(num - 1)
+    if num == 0:
+        return 1
+    else:
+        return num * factorial(num - 1)
 
 # Call the main function.
 main()
