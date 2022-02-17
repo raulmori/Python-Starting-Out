dae178c08885f58d0988e85f69123ecc225633b1
raulmori <47406865+raulmori@users.noreply.github.com>
2022-02-16 20:59:59 -0500

    Create Lec14 Ex1.py

---
+++ Lec14 Ex1.py
@@ -0,0 +1,12 @@
+# This program displays an empty window.
+import tkinter
+
+def main():
+    # Create the main window widget.
+    main_window = tkinter.Tk()
+
+    # Enter the tkinter main loop.
+    tkinter.mainloop()
+
+# Call the main function.
+main()
