b4e3bd8180b7ff2f49f4adb2961d52ae1228960a
raulmori <47406865+raulmori@users.noreply.github.com>
2022-02-16 20:10:00 -0500

    Update Lec13 Ex5.py

--- Lec13 Ex5.py
+++ Lec13 Ex5.py
@@ -2,21 +2,21 @@
 # from the Fibonacci series.
 
 def main():
-print('The first 10 numbers in the')
-print('Fibonacci series are:')
+    print('The first 10 numbers in the')
+    print('Fibonacci series are:')
 
-for number in range(1, 11):
-print(fib(number))
+    for number in range(1, 11):
+        print(fib(number))
 
 # The fib function returns the nth number
 # in the Fibonacci series.
 def fib(n):
-if n == 0:
-return 0
-elif n == 1:
-return 1
-else:
-return fib(n - 1) + fib(n - 2)
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
 
 # Call the main function.
 main()
